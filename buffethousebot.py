b2fd5e29f4919bcf229f6d8d566360611818a9c6
Swapratim <Swapratim@users.noreply.github.com>
2017-11-24 11:16:59 +0100

    Add files via upload

--- buffethousebot.py
+++ buffethousebot.py
@@ -83,6 +83,8 @@ def welcome():
     dataall = entry.get('data')
     print ("dataall:::::::::::")
     print (dataall)
+    id1 = data.get('originalRequest').get('data').get('sender').get('id')
+    print (id1)
     sender = dataall.get('sender')
     sender ("sender::::::::::")
     print (sender)
